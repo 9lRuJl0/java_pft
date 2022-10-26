b481fdadc33e7537e173c9de750c2a4cb56c9887
9lRuJl0 <116034611+9lRuJl0@users.noreply.github.com>
2022-10-26 23:56:44 +0300

    Поменял умножение на в формуле последние скобки на знак минус

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -6,7 +6,7 @@ public class Point {
 
 
     public static double distance(Point p1, Point p2) {
-        double distance = Math.sqrt ((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y ) * (p2.y * p1.y));
+        double distance = Math.sqrt ((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y ) * (p2.y - p1.y));
         return distance;
     }
     public Point(double x, double y) {
