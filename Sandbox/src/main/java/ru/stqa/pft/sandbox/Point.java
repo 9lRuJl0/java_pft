8770ac02c9f0b3b6ec9d8b755d0275b8d72ce806
9lRuJl0 <116034611+9lRuJl0@users.noreply.github.com>
2022-10-29 15:50:28 +0300

    Удалил папку Build + делал д/з

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -11,12 +11,17 @@ public class Point {
         double distance = Math.sqrt ((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y ) * (p2.y - p1.y));
         return distance;
     }
+ //   public static double distanse() {
+ //       double distance = Math.sqrt ((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y ) * (p2.y - p1.y));
+
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
+        Point p1 = new Point(4, 5);
+        Point p2 = new Point(6, 7);
 
     }
 }
