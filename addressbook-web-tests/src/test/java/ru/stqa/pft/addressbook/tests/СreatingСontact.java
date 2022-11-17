55b5c9c7b5b4c061e6c503c1fb6167ad77c8ceea
9lRuJl0 <116034611+9lRuJl0@users.noreply.github.com>
2022-11-18 00:18:39 +0300

    Задание №4: Сделать тесты для создания групп и контактов в адресной книге

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/СreatingСontact.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/СreatingСontact.java
@@ -18,44 +18,85 @@ public class СreatingСontact {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
+    login("Admin", "secret");
 
 
   }
 
-  @Test
-  public void testCreatingContact() throws Exception {
-    wd.get("http://localhost/addressbook/");
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).sendKeys("Admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.id("LoginForm")).click();
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//input[@value='Login']")).click();
-    wd.findElement(By.linkText("add new")).click();
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Tomas");
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Anderson");
-    wd.findElement(By.name("nickname")).click();
-    wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys("NEO");
-    wd.findElement(By.name("company")).click();
-    wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys("MetaCortex");
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys("312-555-0690");
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("test@test.com");
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  @Test
+  public void testCreatingContact() throws Exception {
+
+    gotoAddNew();
+    enterName("Tomas");
+    enterLastName("Anderson");
+    enterCompany();
+    enterNickname();
+    enterTelephone("312-555-0690");
+    enterEmail("test@test.com");
+    enterSave();
+    goOut();
+  }
+
+  private void goOut() {
     wd.findElement(By.linkText("home")).click();
     wd.findElement(By.linkText("Logout")).click();
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("Admin");
+  }
+
+  private void enterSave() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  private void enterEmail(String email) {
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(email);
+  }
+
+  private void enterTelephone(String telephone) {
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(telephone);
+  }
+
+  private void enterNickname(String nickname) {
+    wd.findElement(By.name("nickname")).click();
+    wd.findElement(By.name("nickname")).clear();
+    wd.findElement(By.name("nickname")).sendKeys(nickname);
+  }
+
+  private void enterCompany(String company) {
+    wd.findElement(By.name("company")).click();
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(company);
+  }
+
+  private void enterLastName(String lastname) {
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(lastname);
+  }
+
+  private void enterName(String firstname) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(firstname);
+  }
+
+  private void gotoAddNew() {
+    wd.findElement(By.linkText("add new")).click();
   }
 
   @AfterMethod(alwaysRun = true)
