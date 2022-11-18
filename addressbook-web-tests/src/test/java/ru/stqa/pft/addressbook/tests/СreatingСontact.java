72575bb712b8b1f9c789050b890e99f372906706
9lRuJl0 <116034611+9lRuJl0@users.noreply.github.com>
2022-11-18 14:28:28 +0300

    Перес ContactData в папку model

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/СreatingСontact.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/СreatingСontact.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.util.concurrent.TimeUnit;
 
