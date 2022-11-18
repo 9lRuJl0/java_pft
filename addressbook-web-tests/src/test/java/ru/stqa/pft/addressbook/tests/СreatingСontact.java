90907fdcceb00c85bb3671b6373cbb60aeeb7472
9lRuJl0 <116034611+9lRuJl0@users.noreply.github.com>
2022-11-18 17:19:59 +0300

    Для Алексея после добавления вспомогательных методов тесты не запускаюся

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/СreatingСontact.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/СreatingСontact.java
@@ -1,39 +1,9 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-import java.util.concurrent.TimeUnit;
-
-public class СreatingСontact {
-  private WebDriver wd;
-
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
-    login("Admin", "secret");
-
-
-  }
-
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.id("LoginForm")).click();
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
+public class СreatingСontact extends TestBase {
 
   @Test
   public void testCreatingContact() throws Exception {
@@ -44,86 +14,6 @@ public class СreatingСontact {
     goOut();
   }
 
-  private void fillFormContact(ContactData contactData) {
-    enterName(contactData.getFirstname());
-    enterLastName(contactData.getLastname());
-    enterCompany(contactData.getCompany());
-    enterNickname(contactData.getNickname());
-    enterTelephone(contactData.getTelephone());
-    enterEmail(contactData.getEmail());
-  }
 
-  private void goOut() {
-    wd.findElement(By.linkText("home")).click();
-    wd.findElement(By.linkText("Logout")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("Admin");
-  }
 
-  private void enterSave() {
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-  }
-
-  private void enterEmail(String email) {
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(email);
-  }
-
-  private void enterTelephone(String telephone) {
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(telephone);
-  }
-
-  private void enterNickname(String nickname) {
-    wd.findElement(By.name("nickname")).click();
-    wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys(nickname);
-  }
-
-  private void enterCompany(String company) {
-    wd.findElement(By.name("company")).click();
-    wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys(company);
-  }
-
-  private void enterLastName(String lastname) {
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(lastname);
-  }
-
-  private void enterName(String firstname) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(firstname);
-  }
-
-  private void gotoAddNew() {
-    wd.findElement(By.linkText("add new")).click();
-  }
-
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.quit();
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
 }
