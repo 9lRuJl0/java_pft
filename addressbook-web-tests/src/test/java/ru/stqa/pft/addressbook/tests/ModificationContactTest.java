95dcce57bdd04d61e136b55a0e255ba741e40a5b
9lRuJl0 <116034611+9lRuJl0@users.noreply.github.com>
2022-12-11 18:33:28 +0300

    Задание №8: Реализовать проверку и обеспечение предусловий в тестах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ModificationContactTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ModificationContactTest.java
@@ -8,10 +8,13 @@ public class ModificationContactTest  extends TestBase {
     @Test
     public void testModificationContact() {
 
+        if (! app.getContactHelper().isThereAContact()) {
+            app.getContactHelper().createContact(new ContactData("Tomas", "Anderson", "NEO", "MetaCortex", "312-555-0690", "test@test.com", "test1"));
+        }
         app.getNavigationHelper().gotoHomePage();
         app.getContactHelper().selectId();
         app.getContactHelper().selectEdit();
-        app.getContactHelper().fillContactForm(new ContactData("Tomas", "Anderson", "NEO", "MetaCortex", "312-555-0690", "test@test.com", null), false);
+        app.getContactHelper().fillContactForm(new ContactData("Ярослав", "Сорокин", "9RuJl0", "MetaCortex", "312-555-0690", "test@test.com", null), false);
         app.getContactHelper().selectUpdate();
         app.getNavigationHelper().gotoHomePage();
         app.getContactHelper().gotoLogOut();
