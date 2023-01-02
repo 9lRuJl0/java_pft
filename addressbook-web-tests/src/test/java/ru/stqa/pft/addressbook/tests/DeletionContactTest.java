65e13a9e07526b02d76b2dac570c21fda7c34c15
9lRuJl0 <116034611+9lRuJl0@users.noreply.github.com>
2023-01-03 01:06:12 +0300

    Доработал Д/З №8

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeletionContactTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeletionContactTest.java
@@ -2,11 +2,16 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class DeletionContactTest extends TestBase {
 
     @Test
     public void testDeletionContact() {
+        app.getNavigationHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
         if (! app.getContactHelper().isThereAContact()) {
             app.getContactHelper().createContact(new ContactData("Tomas", "Anderson", "NEO", "MetaCortex", "312-555-0690", "test@test.com", "test1"));
         }
