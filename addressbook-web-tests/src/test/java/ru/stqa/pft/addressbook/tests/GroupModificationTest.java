1c6a72c4c3e619059204ac3e01278d6291453e45
9lRuJl0 <116034611+9lRuJl0@users.noreply.github.com>
2023-01-07 16:54:35 +0300

    Сделал лекцию 4.5 все заработало тесты по удалению и модификации группы не падают

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupModificationTest extends TestBase {
     String groupName = "test3";
     @Test
@@ -11,16 +13,16 @@ public class GroupModificationTest extends TestBase {
     public void testGroupModification() {
 
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
         if (! app.getGroupHelper().isThereAGroup(groupName)) {
             app.getGroupHelper().createGroup(new GroupData(groupName, null, null));
         }
-        app.getGroupHelper().selectGroup(before - 1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData(groupName, "test2", "test3"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size());
     }
 }
