ef578e0a64edb10d6116e03def7ee4eebd2a6a22
9lRuJl0 <116034611+9lRuJl0@users.noreply.github.com>
2023-01-30 23:20:25 +0300

    Лекция 6.10. Описание конфигурации тестового стенда

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -7,26 +7,34 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
+    private final Properties properties;
     public WebDriver wd;
-
     public ContactHelper contactHelper;
     public SessionHelper sessionHelper;
-
     public NavigationHelper navigationHelper;
-
     public GroupHelper groupHelper;
     private String browser;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
+        properties = new Properties();
+
     }
 
-    public void init() {
+    public void init() throws IOException {
+
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/java/ru/stqa/pft/addressbook/resources/%s.properties", target))));
+
         if (Objects.equals(browser, BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         } else if (Objects.equals(browser, BrowserType.CHROME)) {
@@ -37,11 +45,11 @@ public class ApplicationManager {
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 //        wd.manage().timeouts().pageLoadTimeout(10,
 //                TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/");
+        wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
-        sessionHelper.login("Admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
         contactHelper = new ContactHelper(wd);
     }
 
